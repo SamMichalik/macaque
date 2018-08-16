cf75af300d746c89f753b20526bae9bb747fc37a
Samuel Michalik <samuelmichalik@yahoo.com>
2018-08-16 16:53:40 +0200

    add attention fetching and displaying on event

--- macaque/static/script.js
+++ macaque/static/script.js
@@ -4,7 +4,7 @@ var image = document.getElementById('image');
 initialize();
 
 function initialize() {
-  var caption = document.querySelectorAll('th.caption');
+  var caption = getCaptionNodes();
   var form = document.querySelector('form');
 
   caption.forEach(word => {
@@ -22,10 +22,12 @@ function initialize() {
     var init = { method: 'POST',
                  body: formData };
 
-    fetch('/upload', init).then(response => {
+    fetch('/caption', init).then(response => {
       return response.json()
     }).then(json => {
+      // display new caption
       var tr = document.querySelector('tr.caption'),
+          length = json.length,
           th,
           i;
 
@@ -36,9 +38,25 @@ function initialize() {
         tr.appendChild(th);
       }
 
-      document.querySelectorAll('th.caption').forEach(word => {
-        word.addEventListener('mouseover', () => { highligthWord(word); });
-        word.addEventListener('mouseout', () => { unhighlightWord(word); });
+      // fetch alphas
+      fetchImages(length).then(urls => {
+        var url = image.getAttribute('src');
+        var nodes = getCaptionNodes();
+
+        for (var i = length-1; i >= 0; i--) {
+          let ix = i;
+
+          nodes[ix].addEventListener('mouseover', () => {
+            image.setAttribute('src', urls[ix]);
+            highligthWord(nodes[ix]);
+          })
+
+          nodes[ix].addEventListener('mouseout', () => {
+            image.setAttribute('src', url);
+            unhighlightWord(nodes[ix]);
+          })
+        }
+
       })
     });
   });
@@ -67,4 +85,38 @@ function hideCaption() {
   while ((child = tr.lastElementChild) !== null) {
     tr.removeChild(child);
   }
+}
+
+function fetchImages(imgCount) {
+
+  return fetch('/alphas')
+  .then(response => {
+    return response.arrayBuffer();
+
+  }).then( arrBuff => {
+
+    let bLen = arrBuff.byteLength;
+    // 2 bytes to store the length of each image
+    let metaLen = 2 * imgCount;
+    // get a view of the metadata
+    let dView = new DataView(arrBuff, bLen - metaLen);
+    let view = new Uint8Array(arrBuff);
+    let offset = 0;
+    let imageURLs = new Array(imgCount);
+
+    for (var i = 0; i < imgCount; i++) {
+      // decode the length of the i-th image
+      let l = dView.getUint16(i * 2, false);
+      // extract the image
+      let subArr = view.subarray(offset, offset + l);
+      imageURLs[i] = URL.createObjectURL(new Blob([subArr]));
+      offset += l;
+    }
+
+    return imageURLs;
+  });
+}
+
+function getCaptionNodes() {
+  return document.querySelectorAll('th.caption');
 }
