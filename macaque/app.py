b67897094ca637f7b695d1e00206cf7e8e07db08
Samuel Michalik <samuelmichalik@yahoo.com>
2018-08-16 16:06:26 +0200

    add working version of attention alpha visualizations

--- macaque/app.py
+++ macaque/app.py
@@ -1,26 +1,50 @@
 import json
+from io import BytesIO
+
 from flask import Flask, render_template, request
 from logic import Model
 
 
 APP = Flask(__name__)
-APP.config["model"] = None
+APP.config['model'] = None
+
 
 @APP.route('/', methods=['GET'])
 def root():
-    caption = APP.config["model"].generate_caption('static/japanese_macaque.jpg')
+    caption = APP.config['model'].generate_caption('static/japanese_macaque.jpg')
     image = "/static/japanese_macaque.jpg"
     alt = "A Japanese Macaque in a hot spring."
     return render_template('root.html', caption=caption, image=image, alt=alt)
 
-@APP.route('/upload', methods=['POST'])
+@APP.route('/caption', methods=['POST'])
 def upload():
     fname = request.files['input-file'].filename
     request.files['input-file'].save('static/' + fname)
-    caption = APP.config['model'].generate_caption('static/' + fname)
+    caption, alphas = APP.config['model'].get_result('static/' + fname)
     return json.dumps(caption)
+
+@APP.route('/alphas', methods=['GET'])
+def respond_alphas():
+    imgs = APP.config['model'].get_result_images()
+    blob = BytesIO()
+    lens = [], prev = 0
+
+    # write all the images to a byte stream
+    for i in imgs:
+        i.save(blob, "JPEG")
+        size = blob.tell() # total number of bytes
+        lens.append(size - prev)
+        prev = size
+
+    # add metadata for decoding
+    for l in lens:
+        for b in l.to_bytes(2, byteorder='big')
+            blob.write(b)
+
+    return blob.getvalue()
+
 
 if __name__ == "__main__":
     model = Model()
-    APP.config["model"] = model
+    APP.config['model'] = model
     APP.run()
--- macaque/logic.py
+++ macaque/logic.py
@@ -1,14 +1,27 @@
+import random
+
+import numpy as np
+#from skimage.transform import rescale
+from PIL import Image, ImageFilter
 from neuralmonkey.readers.image_reader import single_image_for_imagenet
 from neuralmonkey.experiment import Experiment
 from neuralmonkey.dataset.dataset import Dataset
+
+import matplotlib.pyplot as plt
 
 class Model():
 
     def __init__(self):
         self.exp = Experiment(config_path='../models/captioning_1.ini')
         self.exp.build_model()
+        self.input_image_path = ""
+        self.caption = []
+        self.alphas = []
 
     def generate_caption(self, rel_path):
+        # careful here
+        self.input_image_path = rel_path
+
         img = single_image_for_imagenet(path=rel_path,
                                         target_width=224,
                                         target_height=224,
@@ -23,3 +36,58 @@ class Model():
             print(rerr)
             print("Have you trained your model?")
             exit(1)
+
+    def get_result(self, rel_path):
+        self.caption = self.generate_caption(rel_path)
+        self.alphas = get_dummy_alphas(500, 800, size=len(self.caption))
+        return (self.caption, self.alphas)
+
+    def get_result_images(self):
+        res = []
+        ori = Image.open(self.input_image_path)
+
+        for alp in self.alphas:
+            img = Image.fromarray(alp)
+            img = rescale_and_smooth(img, 1)
+            new = apply_attention_mask(ori, img)
+            res.append(new)
+
+        return res
+
+def get_dummy_alphas(height=14, width=14, size=1):
+    res = np.zeros((size, height, width), dtype=np.uint8)
+
+    for k in range(size):
+        arr = np.zeros((height, width))
+        u = random.randint(0, height - 1)
+        v = random.randint(0, width - 1)
+
+        for i in range(height):
+            for j in range(width):
+                arr[i, j] = height**2 + width**2 - (i - u)**2 - (j - v)**2
+
+        max = np.amax(arr)
+        arr = arr / max
+        res[k] = np.uint8(225 * arr)
+
+    return res
+
+def rescale_and_smooth(pil_image, scale=16, smooth=True):
+    w = pil_image.width
+    h = pil_image.height
+    n_img = pil_image.resize((scale * w, scale * h))
+    n_img = n_img.filter(ImageFilter.GaussianBlur(5))
+    return n_img
+
+def apply_attention_mask(orig_pil_img, mask_pil_img, alpha_channel=0.8):
+    assert (orig_pil_img.height == mask_pil_img.height) and \
+        (orig_pil_img.width == mask_pil_img.width)
+    assert alpha_channel <= 1.
+
+    mask = mask_pil_img.convert('RGBA')
+    alpha = int(255 * alpha_channel)
+    mask.putalpha(alpha)
+
+    cp = orig_pil_img.copy()
+    cp.paste(mask, (0,0), mask)
+    return cp
